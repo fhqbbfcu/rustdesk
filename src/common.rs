e238e8f491c4507bc9f249246a9614a262956292
fhqbbfcu <615147584@qq.com>
2025-11-30 22:03:18 +0800

    修改子模块仓库地址和API服务地址20251130

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://121.4.104.2:21114".to_owned()
 }
 
 #[inline]
